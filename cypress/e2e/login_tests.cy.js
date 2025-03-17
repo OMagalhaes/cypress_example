d0d087e53cb8ee7e94b8e43472ff55ceef8dd321
Jose Magalhaes <jose.p.magalhaes19@gmail.com>
2025-03-17 19:40:20 +0000

    Finished Login Tests - Positive and Negative Scenarios

--- cypress/e2e/login_tests.cy.js
+++ cypress/e2e/login_tests.cy.js
@@ -1,11 +1,42 @@
 import LoginPage from "../pages/loginPage";
-import appHeader from "../pages/appHeader";
+import AppHeader from "../pages/appHeader";
 import testData from "../test_data/data.json";
 
+beforeEach( () => {
+  cy.visit('https://www.saucedemo.com/');
+});
 
-describe('Login Test', () => {
-  it('Valid Login', () => {
+describe('Login Tests', () => {
+
+  it('Test Login - Valid Credentials', () => {
     LoginPage.openLoginPage();
     LoginPage.fillCredentialsLogin(testData.validUser, testData.validPassword);
-  })
+    AppHeader.openAppPage();
+  });
+
+  it('Test Login - Missing username', () => {
+    LoginPage.openLoginPage();
+    LoginPage.fillPasswordLogin(testData.validPassword);
+    LoginPage.checkErrorMessage(testData.noUsernameErrorMessage);
+  });
+
+  it('Test Login - Missing password', () => {
+    LoginPage.openLoginPage();
+    LoginPage.fillUsernameLogin(testData.validUser);
+    LoginPage.checkErrorMessage(testData.noPasswordErrorMessage);
+  });
+
+  it('Test Login - Non-existent user', () => {
+    LoginPage.openLoginPage();
+    LoginPage.fillCredentialsLogin(testData.invalidUser, testData.validPassword);
+    LoginPage.checkErrorMessage(testData.invalidUserErrorMessage);
+  });
+
+  it('Test Login - Locked user', () => {
+    LoginPage.openLoginPage();
+    LoginPage.fillCredentialsLogin(testData.lockedUser, testData.validPassword);
+    LoginPage.checkErrorMessage(testData.lockedUserErrorMessage);
+  });
+
+
 })
--- cypress/pages/loginPage.js
+++ cypress/pages/loginPage.js
@@ -3,14 +3,29 @@ import { elements as el } from "./loginPage_elements";
 class loginPage {
 
     openLoginPage() {
-        cy.visit('https://www.saucedemo.com/');
         cy.get(el.loginPageTitle).contains('Swag Labs');
+    }
+
+    fillUsernameLogin(loginValue) {
+        cy.get(el.userNameEditBox).type(loginValue);
+        cy.get(el.loginButton).click();
+    }
+
+    fillPasswordLogin(passwordValue) {
+        cy.get(el.passwordEditBox).type(passwordValue);
+        cy.get(el.loginButton).click();
     }
 
     fillCredentialsLogin(loginValue, passwordValue) {
         cy.get(el.userNameEditBox).type(loginValue);
         cy.get(el.passwordEditBox).type(passwordValue);
         cy.get(el.loginButton).click();
+    }
+
+    checkErrorMessage(errorMessageValue) {
+        cy.get(el.userNameEditBox).should('have.class', 'error');
+        cy.get(el.passwordEditBox).should('have.class', 'error');
+        cy.get(el.loginErrorLabel).should('have.text', errorMessageValue);
     }
 
 }
