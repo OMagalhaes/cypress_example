a52dff063366a4172f0c03c69b43f2793a322be0
Jose Magalhaes <jose.p.magalhaes19@gmail.com>
2025-03-18 00:32:40 +0000

    added mochaawesome reporter support

--- cypress/e2e/happy_flow.cy.js
+++ cypress/e2e/happy_flow.cy.js
@@ -11,31 +11,37 @@ before( () => {
 
 describe('Happy Path automation', () => {
 
-  it('Login with valid Credentials', () => {
-    LoginPage.openLoginPage();
-    LoginPage.fillCredentialsLogin(testData.validUser, testData.validPassword);
-    AppHeader.openCatalogPage();
-    CatalogPage.addTwoRandomItemsToCart();
-    AppHeader.checkCartQuantity("2");
-    AppHeader.clickCartPageButton();
-    AppHeader.openCartPage();
-    CartPage.removeRandomItemFromCart();
-    AppHeader.checkCartQuantity("1");
-    CartPage.clickCheckoutButton();
-    AppHeader.openCheckoutPage();
-    CheckoutPage.fillAndVerifyFirstName(testData.checkoutFirstName);
-    CheckoutPage.fillAndVerifyLastName(testData.checkoutLastName);
-    CheckoutPage.fillAndVerifyPostalCode(testData.checkoutPostalCode);
-    CheckoutPage.clickContinueCheckout();
-    AppHeader.openCheckoutPageOverview();
-    AppHeader.checkCartQuantity("1");
-    CheckoutPage.confirmCartItemsAmount(1);
-    CheckoutPage.confirmCheckoutTotalAmountLabel(testData.checkoutSummaryTotalAmountLabel);
-    CheckoutPage.clickFinishCheckoutButton();
-    AppHeader.openCheckoutPageSuccess();
-    AppHeader.checkItemsRemovedFromCart();
-    CheckoutPage.checkoutFinishedSuccessfully(testData.checkoutSuccessMessage);
-    AppHeader.openCatalogPage();
+    it('Happy Path Test Flow', () => {
+        //Login into the application
+        LoginPage.openLoginPage();
+        LoginPage.fillCredentialsLogin(testData.validUser, testData.validPassword);
+        AppHeader.openCatalogPage();
+        //Add items to cart on catalog page and go to cart page
+        CatalogPage.addTwoRandomItemsToCart();
+        AppHeader.checkCartQuantity("2");
+        AppHeader.clickCartPageButton();
+        AppHeader.openCartPage();
+        //Interact with items on cart and go to checkout page
+        CartPage.removeRandomItemFromCart();
+        AppHeader.checkCartQuantity("1");
+        CartPage.clickCheckoutButton();
+        AppHeader.openCheckoutPage();
+        //Fill checkout information and continue
+        CheckoutPage.fillAndVerifyFirstName(testData.checkoutFirstName);
+        CheckoutPage.fillAndVerifyLastName(testData.checkoutLastName);
+        CheckoutPage.fillAndVerifyPostalCode(testData.checkoutPostalCode);
+        CheckoutPage.clickContinueCheckout();
+        //View Checkout Summary Page
+        AppHeader.openCheckoutPageOverview();
+        AppHeader.checkCartQuantity("1");
+        CheckoutPage.confirmCartItemsAmount(1);
+        CheckoutPage.confirmCheckoutTotalAmountLabel(testData.checkoutSummaryTotalAmountLabel);
+        CheckoutPage.clickFinishCheckoutButton();
+        //Finish checkout successfully and return to catalog page
+        AppHeader.openCheckoutPageSuccess();
+        AppHeader.checkItemsRemovedFromCart();
+        CheckoutPage.checkoutFinishedSuccessfully(testData.checkoutSuccessMessage);
+        AppHeader.openCatalogPage();
   });
 
 })
